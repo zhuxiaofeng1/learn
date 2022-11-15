fc2be1b6fddebcd1f34d8dc8a77e269568974d16
zhuxiaofeng1 <1432182977@qq.com>
2022-11-15 17:41:42 +0800

    vue3知识总结

--- web/vue3/样例/Vue3/src/components/Transtion.vue
+++ web/vue3/样例/Vue3/src/components/Transtion.vue
@@ -110,4 +110,13 @@ export default {
     transform: translate3d(4px, 0, 0);
   }
 }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
